d73fc7d80c07d08661cbe783392906bfccf038eb
Navlesh Yadav <119070031+Navleshkumar-Yadav@users.noreply.github.com>
2025-08-04 21:38:36 +0530

    Delete backend directory

--- backend/src/routes/complexityAnalysis.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const complexityRouter = express.Router();
-const userMiddleware = require('../middleware/userMiddleware.js');
-const { analyzeComplexity } = require('../controllers/complexityAnalysis.js');
-
-complexityRouter.post('/analyze', userMiddleware, analyzeComplexity);
-
-module.exports = complexityRouter;
