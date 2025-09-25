31ba17f9757addad08e8a05b00967589fb9c5cba
Navlesh Yadav <119070031+Navleshkumar-Yadav@users.noreply.github.com>
2025-09-25 20:15:18 +0530

    Delete backend directory

--- backend/src/routes/aiChatting.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const aiRouter =  express.Router();
-const userMiddleware = require("../middleware/userMiddleware.js");
-const solveDoubt = require('../controllers/solveDoubt.js');
-
-aiRouter.post('/chat', userMiddleware, solveDoubt);
-
-module.exports = aiRouter;
